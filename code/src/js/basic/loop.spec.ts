87ccaefeead6c0e19a35f5011d649292c2b9d280
金伟强 <jinweiqiang@coding.net>
2021-11-03 17:52:55 +0800

    feat: loop

---
+++ code/src/js/basic/loop.spec.ts
@@ -0,0 +1,37 @@
+describe('循环', () => {
+  test('for', () => {
+    const fn = jest.fn()
+    for(let i = 0; i < 5; i++) {
+      fn()
+    }
+    expect(fn).toBeCalledTimes(5)
+  })
+
+  test('while', () => {
+    const fn = jest.fn()
+    let i = 0
+    while(i < 5) {
+      fn()
+      i++
+    }
+    expect(fn).toBeCalledTimes(5)
+  })
+
+  test('do while', () => {
+    const fn = jest.fn()
+    let i = 0
+    do {
+      fn()
+      i++
+    } while (i < 5)
+    expect(fn).toBeCalledTimes(5)
+
+    const fn2 = jest.fn()
+    i = 0
+    do {
+      fn2()
+      i++
+    } while (i < 0)
+    expect(fn2).toBeCalledTimes(1)
+  })
+})
