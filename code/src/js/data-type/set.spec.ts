86ac39925cca4a587322de217a69e7e5a060c14f
金伟强 <jinweiqiang@coding.net>
2021-11-08 16:33:19 +0800

    feat: Set has

--- code/src/js/data-type/set.spec.ts
+++ code/src/js/data-type/set.spec.ts
@@ -8,6 +8,13 @@ describe('set', () => {
     expect(Array.from(s)).toEqual([2])
   })
 
+  test('包含检查: has', () => {
+    const s = new Set<string>()
+    s.add('a')
+    expect(s.has('a')).toBe(true)
+    expect(s.has('b')).toBe(false)
+  })
+
   test('不存在相同的', () => {
     const s = new Set<number>()
     s.add(1)
