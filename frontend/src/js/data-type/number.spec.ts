ca0d5826ff0273c9d2fe4cd694ecbfe71795bf8a
金伟强 <jinweiqiang@coding.net>
2021-11-22 13:50:11 +0800

    feat: num max, min

--- frontend/src/js/data-type/number.spec.ts
+++ frontend/src/js/data-type/number.spec.ts
@@ -1,3 +1,5 @@
+import { max } from "lodash"
+
 describe('数字', () => {
   test('数字判断: typeof', () => {
     expect(typeof 3 === 'number').toBe(true)
@@ -34,5 +36,17 @@ describe('数字', () => {
     expect(0.1 + 0.2).not.toBe(0.3)
     expect(0.1 * 0.2).not.toBe(0.02)
     expect(0.2 + 0.2).toBe(0.4)
+  })
+
+  test('最大最小值: Math.max, Math.min', () => {
+    expect(Math.max(1, 2, 3, 4, 5)).toBe(5)
+    expect(Math.max(7)).toBe(7)
+    expect(Math.max('1' as unknown as number, '2' as unknown as number)).toBe(2)
+    // 数组
+    expect(Math.max(...[1, 6.7, 3.5])).toBe(6.7)
+    // 有非数字的情况
+    expect(isNaN(Math.max(1, 'a' as unknown as number))).toBe(true)
+
+    expect(Math.min(1, 2, 3, 4, 5)).toBe(1)
   })
 })
