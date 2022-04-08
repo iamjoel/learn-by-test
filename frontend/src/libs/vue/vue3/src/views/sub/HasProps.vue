da01da11b89d195c37e2a2272e5bb1375e6f5eef
金伟强 <jinweiqiang@coding.net>
2022-04-08 17:57:46 +0800

    add has props file

---
+++ frontend/src/libs/vue/vue3/src/views/sub/HasProps.vue
@@ -0,0 +1,24 @@
+<script setup >
+import {  ref } from 'vue'
+
+const props = defineProps({
+  name: String
+})
+
+const emit = defineEmits(['change'])
+
+const name = ref(props.name)
+const changeName = () => {
+  name.value = name.value === 'joel' ? 'jack' : 'joel'
+  emit('change', name.value)
+}
+</script>
+
+<template>
+  <main>
+    <div>{{name}}</div>
+    <div>
+      <button @click="changeName">改名</button>
+    </div>
+  </main>
+</template>
