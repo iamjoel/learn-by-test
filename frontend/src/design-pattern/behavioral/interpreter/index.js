17386836f080b1ac184d717f669ef3b4f0727014
九彩拼盘 <iamjoel007@gmail.com>
2022-04-19 15:02:11 +0800

    feat: interpreter

---
+++ frontend/src/design-pattern/behavioral/interpreter/index.js
@@ -0,0 +1,20 @@
+export class Sum {
+  constructor(left, right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  interpret() {
+    return this.left.interpret() + this.right.interpret();
+  }
+}
+
+export class Num {
+  constructor(val) {
+    this.val = val;
+  }
+
+  interpret() {
+    return this.val;
+  }
+}
---
+++ frontend/src/design-pattern/behavioral/interpreter/index.spec.js
@@ -0,0 +1,8 @@
+import {Sum, Num} from '.'
+
+describe('interpreter', () => {
+    test('parse sum express', () => {
+        const sumExp = new Sum(new Num(100), new Num(50))
+        expect(sumExp.interpret()).toBe(150);
+    })
+})
